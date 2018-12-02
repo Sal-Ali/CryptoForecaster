f6c0ab39d20fee8605744452815819dc787134b5
Sal-Ali <emailingsal@gmail.com>
2018-12-02 07:14:46 -0500

    Add files via upload

--- CryptoForecaster/decision_maker.py
+++ CryptoForecaster/decision_maker.py
@@ -1,2 +1,17 @@
-# -*- coding: utf-8 -*-
+#load model from bin or model to use for decisions
+
+#use predict of incoming feature to give out a recommendation
+
+#act on recommendation then pass that action to database
+
+
+# THEN implement backtester, and refitter
+    # back-tester essentially evaluates the db data, flips
+    # buy into sell if it was wrong, wrong determined by the
+    # next result, last result changed to buy.
+    # Takes old model data, refits this new data, does cv test to 
+    # get the latest and greatest result and then saves that model 
+    # into model slot
+    
+# FINALLY create driver to tie it all together, multithread (?)
 
